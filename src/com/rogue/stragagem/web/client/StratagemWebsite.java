97146a004dc859f00b204efbe91e96eb9ae4087f
R. Matt McCann <mccann.matt@gmail.com>
2013-03-16 17:15:49 -0400

    Initial layout design finished

---
+++ src/com/rogue/stragagem/web/client/StratagemWebsite.java
@@ -0,0 +1,11 @@
+package com.rogue.stragagem.web.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class StratagemWebsite implements EntryPoint {
+    /** This is the entry point method. */
+    public void onModuleLoad() { }
+}
